b5a12ba883fdbcd727e9a4e2c56bc7f0d8822ec2
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-06-25 09:55:29 +0530

    Add files via upload

---
+++ build/generated/src/org/apache/jsp/ProposalForm_jsp.java
@@ -0,0 +1,244 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class ProposalForm_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html lang=\"az\">\n");
+      out.write("    <head>\n");
+      out.write("\n");
+      out.write("        <meta charset=\"UTF-8\">\n");
+      out.write("        <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n");
+      out.write("        <meta name=\"generator\" content=\"Mobirise v4.6.6, mobirise.com\">\n");
+      out.write("        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, minimum-scale=1\">\n");
+      out.write("\n");
+      out.write("        <meta name=\"description\" content=\"\">\n");
+      out.write("        <title>Home</title>\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets-UserSubmitIdeas/web/assets/mobirise-icons/mobirise-icons.css\">\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets-UserSubmitIdeas/tether/tether.min.css\">\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets-UserSubmitIdeas/bootstrap/css/bootstrap.min.css\">\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets-UserSubmitIdeas/bootstrap/css/bootstrap-grid.min.css\">\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets-UserSubmitIdeas/bootstrap/css/bootstrap-reboot.min.css\">\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets-UserSubmitIdeas/animatecss/animate.min.css\">\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets-UserSubmitIdeas/dropdown/css/style.css\">\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets-UserSubmitIdeas/theme/css/style.css\">\n");
+      out.write("        <link rel=\"stylesheet\" href=\"assets-UserSubmitIdeas/mobirise/css/mbr-additional.css\" type=\"text/css\">\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("        <section class=\"menu cid-qNHKkLoJ6c\" once=\"menu\" id=\"menu2-0\">\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("            <nav class=\"navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm bg-color transparent\">\n");
+      out.write("                <button class=\"navbar-toggler navbar-toggler-right\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n");
+      out.write("                    <div class=\"hamburger\">\n");
+      out.write("                        <span></span>\n");
+      out.write("                        <span></span>\n");
+      out.write("                        <span></span>\n");
+      out.write("                        <span></span>\n");
+      out.write("                    </div>\n");
+      out.write("                </button>\n");
+      out.write("                <div class=\"menu-logo\">\n");
+      out.write("                    <div class=\"navbar-brand\">\n");
+      out.write("                        <span class=\"navbar-caption-wrap\"><a class=\"navbar-caption text-white display-4\" href=\"index.html\">\n");
+      out.write("                                INNERPRENEUR\n");
+      out.write("                            </a></span>\n");
+      out.write("                    </div>\n");
+      out.write("                </div>\n");
+      out.write("                <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">\n");
+      out.write("                    <!--  <ul class=\"navbar-nav nav-dropdown\" data-app-modern-menu=\"true\"><li class=\"nav-item\">\n");
+      out.write("                              <a class=\"nav-link link text-white display-4\" href=\"#\">\n");
+      out.write("                                  Services\n");
+      out.write("                              </a>\n");
+      out.write("                          </li>\n");
+      out.write("                          <li class=\"nav-item\">\n");
+      out.write("                              <a class=\"nav-link link text-white display-4\" href=\"#\">\n");
+      out.write("                                  About Us\n");
+      out.write("                              </a>\n");
+      out.write("                          </li></ul>-->\n");
+      out.write("                    <div class=\"navbar-buttons mbr-section-btn\">\n");
+      out.write("                        <a class=\"btn btn-sm btn-primary display-4\" href=\"Logout.jsp\">\n");
+      out.write("                            <!--<span class=\"btn-icon mbri-mobile mbr-iconfont mbr-iconfont-btn\">-->\n");
+      out.write("                            </span>\n");
+      out.write("                            Logout\n");
+      out.write("                        </a>\n");
+      out.write("                    </div>\n");
+      out.write("                </div>\n");
+      out.write("            </nav>\n");
+      out.write("        </section>\n");
+      out.write("\n");
+      out.write("        <section class=\"mbr-section form1 cid-qNHKq14Nx2\" id=\"form1-1\">\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("            <div class=\"container\">\n");
+      out.write("                <div class=\"row justify-content-center\">\n");
+      out.write("                    <div class=\"title col-12 col-lg-8\">\n");
+      out.write("                        <h2 class=\"mbr-section-title align-center pb-3 mbr-fonts-style display-2\">\n");
+      out.write("                            GIVE YOUR PROPOSALS HERE</h2>\n");
+      out.write("\n");
+      out.write("                    </div>\n");
+      out.write("                </div>\n");
+      out.write("            </div>\n");
+      out.write("            <div class=\"container\">\n");
+      out.write("                <div class=\"row justify-content-center\">\n");
+      out.write("                    <div class=\"media-container-column col-lg-8\" >\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("                        <form class=\"mbr-form\" action=\"GiveProposal\" method=\"post\" enctype=\"multipart/form-data\">\n");
+      out.write("                            <div class=\"row row-sm-offset\">\n");
+      out.write("                                <div class=\"col-md-4 multi-horizontal\" data-for=\"name\">\n");
+      out.write("                                    <div class=\"form-group\">\n");
+      out.write("                                        <label class=\"form-control-label mbr-fonts-style display-7\" for=\"subid\">Submitter Id</label>\n");
+      out.write("                                        <input type=\"text\" class=\"form-control\" name=\"subid\" required=\"\" id=\"subid\">\n");
+      out.write("                                    </div>\n");
+      out.write("                                </div></div>\n");
+      out.write("                            <div class=\"row row-sm-offset\">\n");
+      out.write("                                <div class=\"col-md-4 multi-horizontal\" data-for=\"name\">\n");
+      out.write("                                    <div class=\"form-group\">\n");
+      out.write("                                        <label class=\"form-control-label mbr-fonts-style display-7\" for=\"ideaid\">Idea Id</label>\n");
+      out.write("                                        <input type=\"text\" class=\"form-control\" name=\"ideaid\" required=\"\" id=\"ideaid\">\n");
+      out.write("                                    </div>\n");
+      out.write("                                </div></div>\n");
+      out.write("                            <div class=\"row row-sm-offset\">\n");
+      out.write("                                <div class=\"col-md-4 multi-horizontal\" data-for=\"name\">\n");
+      out.write("                                    <div class=\"form-group\">\n");
+      out.write("                                        <label class=\"form-control-label mbr-fonts-style display-7\" for=\"orgid\">Organisation Id</label>\n");
+      out.write("                                        <input type=\"text\" class=\"form-control\" name=\"orgid\" required=\"\" id=\"orgid\">\n");
+      out.write("                                    </div>\n");
+      out.write("                                </div></div>\n");
+      out.write("                            <div class=\"row row-sm-offset\">\n");
+      out.write("                                <div class=\"col-md-4 multi-horizontal\" data-for=\"ideatitle\">\n");
+      out.write("                                    <div class=\"form-group\">\n");
+      out.write("                                        <label class=\"form-control-label mbr-fonts-style display-7\" for=\"ideatitle\">Idea Title</label>\n");
+      out.write("                                        <input type=\"text\" class=\"form-control\" name=\"ideatitle\" data-form-field=\"ideatitle\" required=\"\" id=\"ideatitle\">\n");
+      out.write("                                    </div>\n");
+      out.write("                                </div></div>\n");
+      out.write("                            <br>\n");
+      out.write("                            <div class=\"row row-sm-offset\">\n");
+      out.write("                                <div class=\"col-md-4 multi-horizontal\" data-for=\"phone\">\n");
+      out.write("                                    <label class=\"form-control-label mbr-fonts-style display-7\" for=\"ideadesc\">Select State</label>\n");
+      out.write("                                    <input type=\"hidden\" name=\"country\" id=\"countryId\" value=\"IN\"/>\n");
+      out.write("                                    <select name=\"state\" class=\"states order-alpha\" id=\"stateId\">\n");
+      out.write("                                        <option value=\"\">State</option>\n");
+      out.write("                                    </select>&emsp;&emsp;\n");
+      out.write("                                    <label class=\"form-control-label mbr-fonts-style display-7\" for=\"ideadesc\">Select City</label>\n");
+      out.write("                                    <select name=\"city\" class=\"cities order-alpha\" id=\"cityId\">\n");
+      out.write("                                        <option value=\"\">Select City</option>\n");
+      out.write("                                    </select>\n");
+      out.write("                                    <script src=\"//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js\"></script> \n");
+      out.write("                                    <script src=\"//geodata.solutions/includes/statecity.js\"></script>\n");
+      out.write("                                </div></div>\n");
+      out.write("                            <br>\n");
+      out.write("                            <div class=\"row row-sm-offset\">\n");
+      out.write("                                <div class=\"col-md-4 multi-horizontal\" data-for=\"phone\">\n");
+      out.write("                                    <label class=\"form-control-label mbr-fonts-style display-7\" for=\"ideadesc\">Upload your presentation:</label>&emsp;\n");
+      out.write("                                    <input type=\"file\" name=\"presentation\" size=\"50\"/>\n");
+      out.write("                                </div></div>\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("                            <span class=\"input-group-btn\">\n");
+      out.write("                                <button href=\"\" type=\"submit\" class=\"btn btn-primary btn-form display-4\">SUBMIT</button>\n");
+      out.write("                            </span>\n");
+      out.write("                        </form>\n");
+      out.write("                    </div>\n");
+      out.write("                </div>\n");
+      out.write("            </div>\n");
+      out.write("        </section>\n");
+      out.write("\n");
+      out.write("        <section once=\"\" class=\"cid-qNHKBnA7Mo\" id=\"footer6-2\">\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("            <div class=\"container\">\n");
+      out.write("                <div class=\"media-container-row align-center mbr-white\">\n");
+      out.write("                    <div class=\"col-12\">\n");
+      out.write("                        <p class=\"mbr-text mb-0 mbr-fonts-style display-7\">\n");
+      out.write("                            © Copyright 2018 Incognitoos - All Rights Reserved\n");
+      out.write("                        </p>\n");
+      out.write("                    </div>\n");
+      out.write("                </div>\n");
+      out.write("            </div>\n");
+      out.write("        </section>\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("        <script src=\"assets-UserSubmitIdeas/web/assets/jquery/jquery.min.js\"></script>\n");
+      out.write("        <script src=\"assets-UserSubmitIdeas/popper/popper.min.js\"></script>\n");
+      out.write("        <script src=\"assets-UserSubmitIdeas/tether/tether.min.js\"></script>\n");
+      out.write("        <script src=\"assets-UserSubmitIdeas/bootstrap/js/bootstrap.min.js\"></script>\n");
+      out.write("        <script src=\"assets-UserSubmitIdeas/smoothscroll/smooth-scroll.js\"></script>\n");
+      out.write("        <script src=\"assets-UserSubmitIdeas/touchswipe/jquery.touch-swipe.min.js\"></script>\n");
+      out.write("        <script src=\"assets-UserSubmitIdeas/viewportchecker/jquery.viewportchecker.js\"></script>\n");
+      out.write("        <script src=\"assets-UserSubmitIdeas/dropdown/js/script.min.js\"></script>\n");
+      out.write("        <script src=\"assets-UserSubmitIdeas/theme/js/script.js\"></script>\n");
+      out.write("        <script src=\"assets-UserSubmitIdeas/formoid/formoid.min.js\"></script>\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("        <div id=\"scrollToTop\" class=\"scrollToTop mbr-arrow-up\"><a style=\"text-align: center;\"><i></i></a></div>\n");
+      out.write("        <input name=\"animation\" type=\"hidden\">\n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
